0c564546d065df4221f6e60f8373fb985517d5e6
TazkiaAdilaAffandie <tazkia_affandie_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:22:55 +0700

    Add files via upload

---
+++ teman.java
@@ -0,0 +1,37 @@
+public class teman {
+    public static void main(String[] args) {
+   //variable -> tipe_data nama_variable = value;
+String name = "Shabrina";
+int age = 16;
+String citacita = "sucessful boss"; 
+System.out.println(name);
+System.out.println(age);
+System.out.println(citacita);
+String name2 = "Najwa Az Zahra"; 
+int age2 = 15;
+String citacita2 = "Young boss";
+System.out.println(name2);
+System.out.println(age2);
+System.out.println(citacita2);
+String name3 = "Kholifatul Ulfa Yulsin";
+int age3 = 15; 
+String citacita3 = "Succesful CEO";
+System.out.println(name3);
+System.out.println(age3);
+System.out.println(citacita3);
+String name4 = "Radisty Kirana Dhisa Aqila";
+int age4 = 15;
+String citacita4 = "Software Engineering";
+System.out.println(name4);
+System.out.println(age4);
+System.out.println(citacita4);
+String name5 = "Salsabila Abhista Raharjo"; 
+int age5 = 16;
+String citacita5 = "Own a company";
+System.out.println(name5);
+System.out.println(age5);
+System.out.println(citacita5);
+
+    }
+    
+}
