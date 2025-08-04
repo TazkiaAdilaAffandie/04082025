329fe951440179af05b80dcd3fade250c55ad76c
TazkiaAdilaAffandie <tazkia_affandie_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:16:42 +0700

    Add files via upload

--- Namafile.java
+++ Namafile.java
@@ -1,5 +1,12 @@
 public class Namafile {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+        //main method
         System.out.println("Tazkia");
+        System.out.println("SMK Telkom Malang");
+        System.out.println("Scholars Indian School");
+        System.out.println("Abdillah Bin Roffii");
+        System.out.println("Septin Handayani");
+        System.out.println("Jadi graphic designer");
+        System.out.println("Drawing");
     }
 }
